0956df802ca30d3feb34ebc69c47158b52edcda6
mohammadtonor <mtonor1368@gmail.com>
2024-02-18 18:43:25 +0330

    complete navbar component and about component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import {About, Header, Footer, Work, Skills, Testimonial} from "./container";
+import  { Navbar } from './components'
+import './App.scss'
 
 const App = () => {
     return (
         <div className='app'>
+            <Navbar />
             <Header />
             <About />
             <Work />
